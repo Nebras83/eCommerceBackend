b147baae1c999627701a1f639906b3df82df7222
Laravel Daily <info@laraveldaily.com>
2023-04-20 15:59:18 +0000

    QuickAdminPanel automatic commit

---
+++ app/Http/Controllers/Admin/HomeController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use LaravelDaily\LaravelCharts\Classes\LaravelChart;
+
+class HomeController
+{
+    public function index()
+    {
+        $settings1 = [
+            'chart_title'           => 'Latest Users',
+            'chart_type'            => 'bar',
+            'report_type'           => 'group_by_date',
+            'model'                 => 'App\Models\User',
+            'group_by_field'        => 'created_at',
+            'group_by_period'       => 'month',
+            'aggregate_function'    => 'count',
+            'filter_field'          => 'created_at',
+            'filter_days'           => '30',
+            'group_by_field_format' => 'Y-m-d H:i:s',
+            'column_class'          => 'col-md-4',
+            'entries_number'        => '5',
+            'translation_key'       => 'user',
+        ];
+
+        $chart1 = new LaravelChart($settings1);
+
+        $settings2 = [
+            'chart_title'           => 'Users Amount',
+            'chart_type'            => 'number_block',
+            'report_type'           => 'group_by_date',
+            'model'                 => 'App\Models\User',
+            'group_by_field'        => 'email_verified_at',
+            'group_by_period'       => 'day',
+            'aggregate_function'    => 'count',
+            'filter_field'          => 'created_at',
+            'filter_days'           => '14',
+            'group_by_field_format' => 'Y-m-d H:i:s',
+            'column_class'          => 'col-md-4',
+            'entries_number'        => '5',
+            'translation_key'       => 'user',
+        ];
+
+        $settings2['total_number'] = 0;
+        if (class_exists($settings2['model'])) {
+            $settings2['total_number'] = $settings2['model']::when(isset($settings2['filter_field']), function ($query) use ($settings2) {
+                if (isset($settings2['filter_days'])) {
+                    return $query->where($settings2['filter_field'], '>=',
+                        now()->subDays($settings2['filter_days'])->format('Y-m-d'));
+                } elseif (isset($settings2['filter_period'])) {
+                    switch ($settings2['filter_period']) {
+                        case 'week': $start = date('Y-m-d', strtotime('last Monday'));
+                        break;
+                        case 'month': $start = date('Y-m') . '-01';
+                        break;
+                        case 'year': $start = date('Y') . '-01-01';
+                        break;
+                    }
+                    if (isset($start)) {
+                        return $query->where($settings2['filter_field'], '>=', $start);
+                    }
+                }
+            })
+                ->{$settings2['aggregate_function'] ?? 'count'}($settings2['aggregate_field'] ?? '*');
+        }
+
+        $settings3 = [
+            'chart_title'           => 'Lastest Users',
+            'chart_type'            => 'latest_entries',
+            'report_type'           => 'group_by_date',
+            'model'                 => 'App\Models\User',
+            'group_by_field'        => 'email_verified_at',
+            'group_by_period'       => 'day',
+            'aggregate_function'    => 'count',
+            'filter_field'          => 'created_at',
+            'group_by_field_format' => 'Y-m-d H:i:s',
+            'column_class'          => 'col-md-12',
+            'entries_number'        => '10',
+            'fields'                => [
+                'id'         => '',
+                'name'       => '',
+                'email'      => '',
+                'created_at' => '',
+            ],
+            'translation_key' => 'user',
+        ];
+
+        $settings3['data'] = [];
+        if (class_exists($settings3['model'])) {
+            $settings3['data'] = $settings3['model']::latest()
+                ->take($settings3['entries_number'])
+                ->get();
+        }
+
+        if (! array_key_exists('fields', $settings3)) {
+            $settings3['fields'] = [];
+        }
+
+        return view('home', compact('chart1', 'settings2', 'settings3'));
+    }
+}
---
+++ config/panel.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'date_format'         => 'Y-m-d',
+    'time_format'         => 'H:i:s',
+    'primary_language'    => 'en',
+    'available_languages' => [
+        'en' => 'English',
+        'de' => 'German',
+        'fr' => 'French',
+        'nl' => 'Dutch',
+        'it' => 'Italian',
+        'ar' => 'Arabic',
+    ],
+    'registration_default_role' => '2',
+
+];
